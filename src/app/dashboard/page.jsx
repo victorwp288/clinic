7583da68441736d83f9b97eccab29fa162c1b7d9
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2024-02-28 19:21:19 +0100

    Add @heroicons/react package to dependencies

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,38 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { createClient } from '@/utils/supabase/client'
+
+
+function Dashboard() {
+
+  const supabase = createClient()
+  const [appointments, setAppointments] = useState([])
+
+  // fetch appointments from db for today in a function
+  async function fetchAppointments() {
+	const { data, error } = await supabase
+	  .from('appointments')
+	  .select('*')
+	  .eq('date', new Date().toLocaleDateString())
+	if (error) console.error('Error fetching appointments:', error)
+	else setAppointments(data)
+  }
+
+  // fetch appointments from db for the next week day
+  async function fetchAppointmentsNextWeek() {
+	const { data, error } = await supabase
+	  .from('appointments')
+	  .select('*')
+	  .eq('date', new Date().toLocaleDateString())
+	if (error) console.error('Error fetching appointments:', error)
+	else setAppointments(data)
+  }
+
+
+  return <div>
+	<h1>Dashboard</h1>
+
+  </div>
+}
+
+export default BookAppointment
